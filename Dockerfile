a14d5ddda0650ad08cd1f2f3ff2826291d06160f
Shubham Suresh Chavan <shubhamchav@cybage.com>
2025-04-08 12:46:20 +0530

    project added

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Stage 1: Build
+FROM golang:1.22 AS builder
+
+# Set the Current Working Directory inside the container
+WORKDIR /app
+
+# Copy go.mod and go.sum files
+COPY go.mod go.sum ./
+
+# Download all dependencies
+RUN go mod download
+
+# Copy the rest of the application code
+COPY . .
+
+# Build the Go app with static linking
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
+
+# Stage 2: Run
+FROM scratch
+
+LABEL maintainer="Shubham Chavan <shubhamchav@cybage.com>"
+
+# Set the Current Working Directory inside the container
+WORKDIR /app
+
+# Copy the built executable from the build stage
+COPY --from=builder /app/app .
+
+# Expose port 8082
+EXPOSE 8082
+
+# Command to run the executable
+ENTRYPOINT ["./app"]
---
+++ main.go
@@ -0,0 +1,138 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	_ "github.com/go-sql-driver/mysql"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"todo_app/handlers"
+// 	"todo_app/services"
+
+// 	"github.com/rs/cors"
+// )
+
+// func main() {
+// 	// Initialize database connection
+// 	dsn := "avnadmin:AVNS_j9dszxAT3x87Lf9xOKb@tcp(mysql-bafa257-shubham1997.g.aivencloud.com:20602)/todo_app"
+// 	db, err := sql.Open("mysql", dsn)
+// 	if err != nil {
+// 		log.Fatal("Error connecting to the database:", err)
+// 	}
+// 	defer db.Close()
+
+// 	// Initialize TodoService
+// 	todoService := &services.TodoServiceImpl{DB: db}
+
+// 	// Initialize TodoHandler with TodoService
+// 	todoHandler := handlers.NewTodoHandler(todoService)
+
+// 	// Define routes
+// 	mux := http.NewServeMux()
+
+// 	mux.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+// 		switch r.Method {
+// 		case http.MethodGet:
+// 			todoHandler.GetAllTodos(w, r)
+// 		case http.MethodPost:
+// 			todoHandler.CreateTodo(w, r)
+// 		default:
+// 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		}
+// 	})
+
+// 	mux.HandleFunc("/todos/", func(w http.ResponseWriter, r *http.Request) {
+// 		if r.Method == http.MethodGet {
+// 			todoHandler.GetTodoByID(w, r)
+// 		} else if r.Method == http.MethodPut {
+// 			todoHandler.UpdateTodo(w, r)
+// 		} else if r.Method == http.MethodDelete {
+// 			todoHandler.DeleteTodo(w, r)
+// 		} else {
+// 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		}
+// 	})
+
+// 	// CORS middleware
+// 	corsHandler := cors.Default().Handler(mux)
+
+// 	// Start the server
+// 	port := ":8080"
+// 	fmt.Println("Server started on port", port)
+// 	log.Fatal(http.ListenAndServe(port, corsHandler))
+// }
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"todo_app/handlers"
+	"todo_app/services"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/cors"
+)
+
+func main() {
+	// Initialize database connection
+	dsn := "avnadmin:AVNS_UWr-wJcElZ5GLHtjWUX@tcp(mysql-1de1931e-tododata.d.aivencloud.com:19068)/todo_app"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+	// Close DB only if connection is successful
+	defer db.Close()
+
+	// Initialize TodoService
+	todoService := &services.TodoServiceImpl{DB: db}
+
+	// Initialize TodoHandler with TodoService
+	todoHandler := handlers.NewTodoHandler(todoService)
+
+	// Define routes
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			todoHandler.GetAllTodos(w, r)
+		case http.MethodPost:
+			todoHandler.CreateTodo(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	mux.HandleFunc("/todos/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			todoHandler.GetTodoByID(w, r)
+		case http.MethodPut:
+			todoHandler.UpdateTodo(w, r)
+		case http.MethodDelete:
+			todoHandler.DeleteTodo(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	// CORS middleware with custom settings
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Frontend ELB
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	// Wrap the router with CORS
+	handler := c.Handler(mux)
+
+	// Start the server
+	port := "0.0.0.0:8080"
+	fmt.Println("Server started on port", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
+
